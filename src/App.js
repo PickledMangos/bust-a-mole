f9cd062a51b631cee31ba8e109e980fdc3736b40
Diana McIntosh <dotteddice@yahoo.com>
2020-10-23 15:31:22 -0400

    undid some refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StartGameScreen } from "./components/StartGameScreen/StartGameScreen.jsx";
 import './App.css';
 
 function App() {
+
+    const [isGameStart, setIsGameStart] = useState(false);
+    console.log('is game started?', isGameStart);
+
+    const handleStartGame = () => {
+        setIsGameStart(true);
+    };
+
   return (
     <div className="App">
-        <div className="StartGameButton">
+        <div className="StartGameButton" onClick={handleStartGame}>
             <StartGameScreen>
             </StartGameScreen>
         </div>
--- src/components/StartGameScreen/StartGameScreen.jsx
+++ src/components/StartGameScreen/StartGameScreen.jsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './StartGameScreen.css';
 
 export const StartGameScreen = () => {
 
-    const [isGameStart, setIsGameStart] = useState(false);
-    console.log('is game started?', isGameStart);
-
-    const handleStartGame = () => {
-        setIsGameStart(true);
-    };
-
     return (
-        <div className="StartGameScreen" onClick={handleStartGame}>
+        <div className="StartGameScreen">
             <p>Click to start</p>
         </div>
     )
