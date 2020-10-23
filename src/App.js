8111d6e0477c006f6817c4e67b98d10b129c0749
Diana McIntosh <dotteddice@yahoo.com>
2020-10-23 15:11:20 -0400

    added state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import StartGameScreen from "./components/StartGameScreen.jsx";
 import './App.css';
 
 function App() {
+
+    const [isGameStart, setIsGameStart] = useState(false);
+
+    function handleStartGame () {
+        setIsGameStart(true);
+        console.log("start game?")
+        console.log(isGameStart)
+
+
+    }
+
   return (
     <div className="App">
-      <StartGameScreen username="peeps">
-      </StartGameScreen>
+        <div className="StartGameButton" onClick={()=>handleStartGame()}>
+            <StartGameScreen username="peeps" >
+            </StartGameScreen>
+
+        </div>
     </div>
   );
 }
--- src/components/StartGameScreen.jsx
+++ src/components/StartGameScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StartGameScreen = (props) => {
     return(
-        <div className="StartGameScreen" onClick={()=>(console.log("CLICK!"))}>
+        <div className="StartGameScreen" >
             <p>Hello, {props.username}</p>
             <p>Click to start</p>
         </div>
