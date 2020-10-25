fac1ec8358c7e39d9818d03b11a281713e1a277d
Diana McIntosh <dmcintosh@kapitus.com>
2020-10-25 12:53:35 -0400

    mole hides when clicked

--- src/components/Mole/Mole.jsx
+++ src/components/Mole/Mole.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Mole.css';
 import moleImage from './mole.png';
 import moleHole from './oval.gif';
 
 export const Mole = (props) => {
-    const { isHidden } = props;
-    return (
-        <div className="mole">
-            <div className="mole__hole">
-                <img src={isHidden ? moleHole : moleImage} alt='mole'></img>
-            </div>
-        </div>
-    );
+  const isHiddenInitalValue = props.isHidden || false;
+  const [isHidden, setIsHidden] = useState(isHiddenInitalValue);
+  const clickHandler = () => (setIsHidden(true));
+  return (
+      <div className="mole">
+          <div className="mole__hole" onClick={clickHandler}>
+              <img src={isHidden ? moleHole : moleImage} alt='mole'></img>
+          </div>
+      </div>
+  );
 };
